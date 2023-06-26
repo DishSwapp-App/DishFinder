4e82c8a3dfcaf0f13ae05ddf7a70a8b6a6d91730
PatrizioTheDev <patickmedley@gmail.com>
2023-06-26 13:09:30 -0400

    GG

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import sys
 from flask import Flask, render_template, request, redirect, url_for
 from recipe_scrapers import scrape_me
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,20 +15,13 @@ def index():
         ingredients = scraper.ingredients()
         directions = scraper.instructions()
 
-        # Redirect the user to the recipe.html page and pass the scraped data to it
-        return redirect(url_for('recipe', ingredients=ingredients, directions=directions))
+        return render_template('index.html', ingredients=ingredients, directions=directions)
 
     return render_template('index.html')
 
 
-@app.route('/recipe')
-def recipe():
-    # Get the scraped data from the URL parameters
-    ingredients = request.args.getlist('ingredients')
-    directions = request.args.get('directions')
-
-    return render_template('recipe.html', ingredients=ingredients, directions=directions)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(debug=True)
